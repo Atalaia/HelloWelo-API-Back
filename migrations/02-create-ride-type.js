dcd96f6c5699dc8a410f5b567263156a04d08aba
Maria Soage <mariasoage@MacBook-Pro-de-Maria.local>
2020-03-23 19:13:07 +0100

    change allowNull in levels, types, status migrations

--- migrations/02-create-ride-type.js
+++ migrations/02-create-ride-type.js
@@ -9,9 +9,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       photoURL: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       createdAt: {
